0e50b275f5d7d24a233a6c74f322657a5d5e6460
Loubna Bouam <bouamloubna5@gmail.com>
2018-10-15 15:43:51 +0200

    add userProfil compononet

---
+++ src/app/user-profile/user-profile.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import {User} from '../common/users';
+@Component({
+  selector: 'app-user-profile',
+  templateUrl: './user-profile.component.html',
+  styleUrls: ['./user-profile.component.css']
+})
+export class UserProfileComponent implements OnInit {
+
+  user: User = new User();
+  masque = true;
+  constructor() { }
+
+  ngOnInit() {
+    this.user.name = 'Bouam';
+    this.user.firstName = 'Loubna';
+    this.user.age = 30;
+    this.user.quote = '';
+    this.user.photo = 'https://randomuser.me/api/portraits/lego/2.jpg';
+  }
+Afficher() {
+  this.masque = !this.masque;
+}
+}
